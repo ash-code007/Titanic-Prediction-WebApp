369caa98f16c3f3d4fba749acfb4bce7f422e38b
Abhishek Hegde <abhishek1998hegde@gmail.com>
2020-09-27 15:37:01 +0530

    Update titanic_webapp.py

--- titanic_webapp.py
+++ titanic_webapp.py
@@ -19,18 +19,26 @@ def main():
     border-style: solid;
     padding:10px;
     }
+    #textspace img {
+    border: 1px solid #ddd;
+    border-radius: 10px;
+    padding: 5px;
+    width: 500px;
+    }
     
     </style>
     
     <div id='textspace'>
         <h1 style="font-family: Helvetica, sans-serif; font-variant: small-caps;">Titanic Survival Prediction</h1>
-        <p> <i>The sinking of the Titanic is one of the most infamous shipwrecks in history.<br/><br/>
+        <img src="https://cdn.wallpapersafari.com/9/99/g7mtvV.jpg" alt="Titanic Image">
+
+        <p> The sinking of the Titanic is one of the most infamous shipwrecks in history.<br/><br/>
         On April 15, 1912, during her maiden voyage, the widely considered “unsinkable” RMS Titanic sank after colliding with an iceberg. 
         Unfortunately, there weren’t enough lifeboats for everyone onboard, resulting in the death of 1502 out of 2224 passengers and crew.
         While there was some element of luck involved in surviving, it seems some groups of people were more likely to survive than others.<br/><br/>
         In this web-app, you can enter the details of an imaginary traveller (ie name, age, gender, socio-economic class, etc). 
         and the app will use the help of an ML algorithm to tell you whether your traveller would have survived the catastrophe or not.
-         </i></p>
+         </p>
     </div>
     
     <h4 style="color:black;text-align:left;">Enter the details of the traveller:</h2>
