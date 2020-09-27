57f706a732ecbad1218bdd09263c8b5f484738d0
Abhishek Hegde <abhishek1998hegde@gmail.com>
2020-09-27 15:46:38 +0530

    Update titanic_webapp.py

--- titanic_webapp.py
+++ titanic_webapp.py
@@ -30,15 +30,23 @@ def main():
     
     <div id='textspace'>
         <h1 style="font-family: Copperplate Gothic Light; font-variant: small-caps;text-align:center;">Titanic Survival Prediction</h1>
-        <img src="https://cdn.wallpapersafari.com/9/99/g7mtvV.jpg" style="align:center;" alt="Titanic Image"></img>
-
-        <p> The sinking of the Titanic is one of the most infamous shipwrecks in history.<br/><br/>
-        On April 15, 1912, during her maiden voyage, the widely considered “unsinkable” RMS Titanic sank after colliding with an iceberg. 
-        Unfortunately, there weren’t enough lifeboats for everyone onboard, resulting in the death of 1502 out of 2224 passengers and crew.
-        While there was some element of luck involved in surviving, it seems some groups of people were more likely to survive than others.<br/><br/>
-        In this web-app, you can enter the details of an imaginary traveller (ie name, age, gender, socio-economic class, etc). 
-        and the app will use the help of an ML algorithm to tell you whether your traveller would have survived the catastrophe or not.
-         </p>
+            <div>
+               <img src="https://cdn.wallpapersafari.com/9/99/g7mtvV.jpg" align="middle" alt="Titanic Image">
+               </img>
+            </div>
+        
+             <div>
+               <p> 
+                  The sinking of the Titanic is one of the most infamous shipwrecks in history.
+                  <br/><br/>
+                  On April 15, 1912, during her maiden voyage, the widely considered “unsinkable” RMS Titanic sank after colliding with an iceberg. 
+                  Unfortunately, there weren’t enough lifeboats for everyone onboard, resulting in the death of 1502 out of 2224 passengers and crew.
+                  While there was some element of luck involved in surviving, it seems some groups of people were more likely to survive than others.
+                  <br/><br/>
+                  In this web-app, you can enter the details of an imaginary traveller (ie name, age, gender, socio-economic class, etc). 
+                  and the app will use the help of an ML algorithm to tell you whether your traveller would have survived the catastrophe or not.
+               </p>
+            </div>
     </div>
     
     <h4 style="color:black;text-align:left;">Enter the details of the traveller:</h2>
